c6f189f80d996513943c7514da21f3a512c5e940
Jastry <blbagony@163.com>
2018-02-12 15:38:55 +0800

    将 url 更新为 连接方式

--- RecruitmentInfo/__init__.py
+++ RecruitmentInfo/__init__.py
@@ -5,48 +5,38 @@ from bs4 import BeautifulSoup
 import urlparse
 import re
 
+def getHtmlInfo(root_url, res_data):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    rsp = urllib2.Request(root_url, headers=headers)
+    content = urllib2.urlopen(rsp).read()
+    soup = BeautifulSoup(content, 'html.parser', from_encoding='utf8')
+    base_url = 'http://jiuye.www.sust.edu.cn'
 
-def getNextUrl(soup, current_url):
-    if current_url is None:
-        return None
-    next_page = soup.find('a', class_='Next')
+    #<tr style="line-height:180%;border-bottom: 1px dotted #cccccc;font-size:10pt;">
+    all_info = soup.find_all('tr', style=re.compile(r'ine-height'))
+    next_info = soup.find('a', class_='Next')
+    next_page = next_info['href']
+    #<a href="/new.jsp?urltype=news.NewsContentUrl&amp;wbtreeid=1003&amp;wbnewsid=11042" target="_blank" title="2017年常州市赴陕西科技大学校园专场招聘会" style="">2017年常州市赴陕西科技大学校园专场招聘会</a>
+    for info in all_info:
+        data = {}
+        url_title = info.find('a', href=re.compile(r'/new.jsp?'))
+        times = info.find('td', width='20%')
+        locals = info.find('td', width='30%')
+        local = locals.get_text()
+        new_url = url_title['href']
+        new_full_url = urlparse.urljoin(base_url, new_url)
+        title = url_title['title']
+        time = times.get_text().strip()
+        data['url'] = new_full_url
+        data['title'] = title
+        data['time'] = time
+        data['local'] = local
+        res_data.append(data)
+
     return next_page
 
 
-def getCurrentInfo(link_list, time_list, title_list, local_list, soup):
-    if soup is None:
-        return None
-
-    link_base = 'http://jiuye.www.sust.edu.cn'
-    links = soup.find_all('a', href=re.compile(r"/new.jsp?"))
-    for link in links:
-        new_url = link['href']
-        title_list.append(link['title'])
-        new_full_url = urlparse.urljoin(link_base, new_url)
-        link_list.append(new_full_url)
-
-    times = soup.find_all('td', width='20%')
-    for time in times:
-        t = time.get_text().strip()
-        time_list.append(t)
-    locals = soup.find_all('td', width='30%')
-    for local in locals:
-        local_list.append(local.get_text())
-
-    return link_list, time_list, title_list, local_list
-
-def getNextSoup(next_page):
-    if next_page is None:
-        return None
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-    rsp = urllib2.Request(next_page, headers=headers)
-    soup = BeautifulSoup(content, 'html.parser', from_encoding='utf8')
-    return soup
-
-
 def outputHtml(res_data):
-    if res_data is None:
-        return None
     fout = open('output.html', 'w')
 
     fout.write("<html>")
@@ -58,10 +48,13 @@ def outputHtml(res_data):
     # ASCII
     for data in res_data:
         fout.write("<tr>")
-        fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+        #<a href="/new.jsp?urltype=news.NewsContentUrl&amp;wbtreeid=1003&amp;wbnewsid=11042" target="_blank" title="2017年常州市赴陕西科技大学校园专场招聘会" style="">2017年常州市赴陕西科技大学校园专场招聘会</a>
+        fout.write("<td>")
+        fout.write("<a href=%s>" % data['url'].encode('utf-8'))
+        fout.write("%s</a>"  % data['title'].encode('utf-8'))
+        fout.write("</td>")
         fout.write("<td>%s</td>" % data['local'].encode('utf-8'))
         fout.write("<td>%s</td>" % data['time'].encode('utf-8'))
-        fout.write("<td>%s</td>" % data['url'].encode('utf-8'))
         fout.write("</tr>")
 
     fout.write("</table>")
@@ -70,40 +63,26 @@ def outputHtml(res_data):
 
     fout.close()
 
+
 if __name__ == '__main__':
     root_url = 'http://jiuye.www.sust.edu.cn/zph.jsp?urltype=tree.TreeTempUrl&wbtreeid=1003'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-    rsp = urllib2.Request(root_url, headers=headers)
-    content = urllib2.urlopen(rsp).read()
-    soup = BeautifulSoup(content, 'html.parser', from_encoding='utf8')
-
-    res_data = {}
-    next_page_base = 'http://jiuye.www.sust.edu.cn/zph.jsp'
-    link_list = list()
-    title_list = list()
-    time_list = list()
-    local_list = list()
-
-
+    base_url = 'http://jiuye.www.sust.edu.cn/zph.jsp'
+    res_data = []
     i = 0
-    while i < 3:
-        getCurrentInfo(link_list, time_list, title_list, local_list, soup)
-        res_data.
-        for link in link_list:
-            print link
-        for title in title_list:
-            print title
-        for time in time_list:
-            print time
-        for local in local_list:
-            print local
-        i+=3
 
-    #outputHtml(res_data)
-    #找到前三页的网址
-    #http://jiuye.www.sust.edu.cn/zph.jsp?a47862t=239&a47862p=2&a47862c=15&urltype=tree.TreeTempUrl&wbtreeid=1003
-    #                                    ?a47862t=239&a47862p=2&a47862c=15&urltype=tree.TreeTempUrl&wbtreeid=1003
-    #首先解析 root_url
-    #得到下一页 url，当前页所有招聘公司 url、 公司名、招聘地点、时间
-    #循环三次，得到前三页的信息
-    #输出为 html 文件
+    while i < 3:
+        root_url = getHtmlInfo(root_url, res_data)
+        root_url = urlparse.urljoin(base_url, root_url)
+        #root_url = new_url
+        print root_url
+        i += 1
+
+
+    outputHtml(res_data)
+        #找到前三页的网址
+        #http://jiuye.www.sust.edu.cn/zph.jsp?a47862t=239&a47862p=2&a47862c=15&urltype=tree.TreeTempUrl&wbtreeid=1003
+        #                                    ?a47862t=239&a47862p=2&a47862c=15&urltype=tree.TreeTempUrl&wbtreeid=1003
+        #首先解析 root_url
+        #得到下一页 url，当前页所有招聘公司 url、 公司名、招聘地点、时间
+        #循环三次，得到前三页的信息
+        #输出为 html 文件
